64e96bbc8647126e432f82a3e50d8c9662cdf4a5
Sastha <shazcool@gmail.com>
2017-12-28 17:41:43 +0530

    replace components and css

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">    
+    <transition name="fade"
+                mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    routeTo (routeName, param) {
+      this.$router.push({name: routeName, params: { text: param }})
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @import './assets/scss/main';
+</style>
